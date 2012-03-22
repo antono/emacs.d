1ac463f5a34b005159ae202475a26fb647fc7a66
Antono Vasiljev <self@antono.info>
2012-03-22 17:36:17 +0300

    Not regexp search by default, no sr-speedbar

--- antono/my-bindings.el
+++ antono/my-bindings.el
@@ -3,8 +3,8 @@
 (global-set-key (kbd "s-i")     'helm-imenu)
 
 (global-set-key (kbd "C-<f10>") 'menu-bar-mode)
-;; (global-set-key (kbd "s-e")     'speedbar-get-focus)
-(global-set-key (kbd "s-e")     'sr-speedbar-toggle)
+(global-set-key (kbd "s-e")     'speedbar-get-focus)
+;; (global-set-key (kbd "s-e")     'sr-speedbar-toggle)
 (global-set-key (kbd "s-b")     'eproject-ibuffer)
 (global-set-key (kbd "s-d")     'dictionary-search)
 (global-set-key (kbd "s-L")     'anything-eproject-files)
@@ -22,10 +22,10 @@
     (switch-to-buffer (other-buffer))))
 
 ;; Use regex searches by default.
-(global-set-key (kbd "C-s")   'isearch-forward-regexp)
-(global-set-key (kbd "\C-r")  'isearch-backward-regexp)
-(global-set-key (kbd "C-M-s") 'isearch-forward)
-(global-set-key (kbd "C-M-r") 'isearch-backward)
+;; (global-set-key (kbd "C-s")   'isearch-forward-regexp)
+;; (global-set-key (kbd "\C-r")  'isearch-backward-regexp)
+;; (global-set-key (kbd "C-M-s") 'isearch-forward)
+;; (global-set-key (kbd "C-M-r") 'isearch-backward)
 
 ;; Font Size
 (define-key global-map (kbd "C-+") 'text-scale-increase)
