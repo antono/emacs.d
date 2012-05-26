a5c9fd2aaa685bc62a95fb8c500584d664fa108b
Antono Vasiljev <self@antono.info>
2012-05-26 14:58:34 +0300

    Bigger font by default...

--- antono/my-ui.el
+++ antono/my-ui.el
@@ -2,12 +2,15 @@
 ;; We'll try to emulate Vim UI and behavior as close as possible
 ;;
 ;; As advanced user You don't need this
-(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
-;; (if (fboundp 'menu-bar-mode) (menu-bar-mode -1)) ;; it's ok in unity
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode 1))
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1)) ;; it's ok in unity
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 
 ;; window history etc
 (winner-mode 1)
+
+;; Desktop
+;; (desktop-save-mode 1)
 
 ;; NO TABS
 (set-default 'indent-tabs-mode nil)
@@ -111,8 +114,9 @@
 
 ;; (set-face-attribute 'default nil :font "Terminus-14")
 ;; (set-face-attribute 'default nil :font "Inconsolata" :height 130)
-;; (set-face-attribute 'default nil :font "Ubuntu Mono" :height 130)
-(set-face-attribute 'default nil :font "Liberation Mono" :height 110)
+(set-face-attribute 'default nil :font "Ubuntu Mono" :height 140)
+;; (set-face-attribute 'default nil :font "Liberation Mono" :height 110)
+;; (set-face-attribute 'default nil :font "Liberation Mono" :height 125)
 
 ;; (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/emacs-color-theme-solarized")
 ;; (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/twilight-theme")
--- init.el
+++ init.el
@@ -1,5 +1,10 @@
 (server-start)
 
+;; Quick UI cleanup
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+
 ;;
 ;; Setup base path relative to this file
 ;;
