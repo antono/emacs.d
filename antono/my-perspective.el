0afb9d1865dd668fcd5d82bcfdebc7457df41668
Antono Vasiljev <self@antono.info>
2012-05-26 16:10:38 +0300

    persp-next, persp-prev and bindings

--- antono/my-perspective.el
+++ antono/my-perspective.el
@@ -2,3 +2,26 @@
 (persp-mode 1)
 (global-set-key (kbd "C-x P") 'persp-switch)
 (global-set-key (kbd "C-x p") 'persp-switch-quick)
+
+
+;;; TODO: push upstream
+(require 'cl)
+
+(defun persp-curr-position (offset)
+  (+ offset
+     (position (persp-name persp-curr)
+               (persp-all-names))))
+
+(defun persp-next ()
+  "Switch to next perspective"
+  (interactive)
+  (persp-switch (nth (persp-curr-position -1) (persp-all-names))))
+
+(defun persp-prev ()
+  "Switch to previous perspective"
+  (interactive)
+  (persp-switch (nth (persp-curr-position +1) (persp-all-names))))
+
+(global-set-key (kbd "C-<tab>") 'persp-next)
+(global-set-key (kbd "C-S-<iso-lefttab>") 'persp-prev)
+
