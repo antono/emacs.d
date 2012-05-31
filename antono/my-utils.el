37252918a54444f800846caae2ba12c57496f229
Antono Vasiljev <self@antono.info>
2012-05-31 17:44:47 +0300

    utils -> what-face

---
+++ antono/my-utils.el
@@ -0,0 +1,5 @@
+(defun what-face (pos)
+  (interactive "d")
+  (let ((face (or (get-char-property (point) 'read-face-name)
+                  (get-char-property (point) 'face))))
+    (if face (message "Face: %s" face) (message "No face at %d" pos))))
